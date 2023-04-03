81d1a860775b189757bb3a4bd4b7500aa6271ce1
Vincent DaSilva <vincent.dasilva@ens-univ.artois.fr>
2023-04-03 09:41:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'developpement_application_s4a01_p2';
+}
