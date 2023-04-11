ff797a14fe44fc9117b1de7fd2c021a48ab46291
Vincent DaSilva <vincent.dasilva@ens-univ.artois.fr>
2023-04-11 12:57:42 +0200

    Ajout debut de jeu

--- src/app/composants/tableau-jeu/tableau-jeu.component.ts
+++ src/app/composants/tableau-jeu/tableau-jeu.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, Observable, of} from "rxjs";
+import {JeuxService} from "../../services/jeux.service";
+import {DataSource} from "@angular/cdk/collections";
+import {Jeu} from "../../../models/jeu";
 
 @Component({
   selector: 'app-tableau-jeu',
-  templateUrl: './tableau-jeu.component.html',
+  template: `
+    <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">
+
+      <!-- nom Column -->
+      <ng-container matColumnDef="nom">
+        <th mat-header-cell *matHeaderCellDef> nom </th>
+        <td mat-cell *matCellDef="let element"> {{element.nom}}</td>
+      </ng-container>
+
+      <!-- description Column -->
+      <ng-container matColumnDef="description">
+        <th mat-header-cell *matHeaderCellDef> description </th>
+        <td mat-cell *matCellDef="let element"> {{element[0].description}}</td>
+      </ng-container>
+
+      <!-- langue Column -->
+      <ng-container matColumnDef="langue">
+        <th mat-header-cell *matHeaderCellDef> langue </th>
+        <td mat-cell *matCellDef="let element"> {{element[0].langue}}</td>
+      </ng-container>
+
+      <!-- categorie id Column -->
+      <ng-container matColumnDef="categorie_id">
+        <th mat-header-cell *matHeaderCellDef> categorie_id </th>
+        <td mat-cell *matCellDef="let element"> {{element[0].categorie_id}}  </td>
+      </ng-container>
+
+      <!-- theme id Column -->
+      <ng-container matColumnDef="theme_id">
+        <th mat-header-cell *matHeaderCellDef> theme_id </th>
+        <td mat-cell *matCellDef="let element"> {{element[0].theme_id}}  </td>
+      </ng-container>
+
+      <!-- La ligne -->
+      <tr mat-header-row *matHeaderRowDef="lesColonnes"></tr>
+      <tr mat-row *matRowDef="let row; columns: lesColonnes;"></tr>
+    </table>
+
+  `,
   styleUrls: ['./tableau-jeu.component.css']
 })
 export class TableauJeuComponent implements OnInit {
 
-  constructor() { }
+  les_jeux: Jeu [] = [];
+  les_jeux$: Observable<Jeu[]> = of([])
+  lesColonnes = ["nom","description","langue","categorie_id","theme_id"]
+  dataSource: DataSourceAsynchro = new DataSourceAsynchro(this.jeuxService)
 
-  ngOnInit(): void {
+  constructor(public jeuxService:JeuxService) {
   }
 
+  ngOnInit(): void {
+    this.dataSource = new DataSourceAsynchro(this.jeuxService)
+    this.dataSource.setData();
+  }
+}
+
+class DataSourceAsynchro extends DataSource<Jeu> {
+  private jeuxSubject = new BehaviorSubject<Jeu[]>([]);
+
+  constructor(private jeuxService: JeuxService) {
+    super();
+  }
+
+  connect(): Observable<Jeu[]> {
+    return this.jeuxSubject.asObservable();
+  }
+
+  disconnect() {
+    this.jeuxSubject.complete();
+  }
+
+  setData() {
+    this.jeuxService.getJeux()
+      .subscribe(jeux => this.jeuxSubject.next(jeux));
+  }
 }
--- src/app/services/jeux.service.ts
+++ src/app/services/jeux.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {catchError, map, Observable, of} from "rxjs";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {catchError, map, Observable, of, tap} from "rxjs";
 import {Jeu} from "../../models/jeu";
 import {AuthService} from "./auth.service";
 
@@ -15,9 +15,11 @@ export class JeuxService {
     Observable<Jeu[]> {
     const url = 'http://127.0.0.1:8000/api/jeu/indexVisiteur';
     const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json',
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': 'Bearer '+this.authService.userValue.jwtToken})
+        'Authorization': 'Bearer ' + this.authService.userValue.jwtToken
+      })
     };
     return this.http.get<any>(url, httpOptions)
       .pipe(
@@ -28,4 +30,27 @@ export class JeuxService {
         }),
       );
   }
+
+  getJeux(sort: string = 'asc', nb_joueur_min: number = 2): Observable<Jeu[]> {
+      const url = 'http://127.0.0.1:8000/api/jeu/indexAdherent';
+      let params = new HttpParams();
+      params = params.append('sort',sort);
+      params = params.append('nb_joueur_min',nb_joueur_min);
+      const httpOptions = {
+        headers: new HttpHeaders({'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer '+this.authService.userValue.jwtToken
+        }),
+        params: params
+      };
+      return this.http.get<any>(url, httpOptions).pipe(
+          map(res => res.data as Jeu[]),
+          tap(res=> console.log('hey : ',res)),
+          catchError(err => {
+            console.log('Erreur http : ', err);
+            return of([]);
+          }),
+        );
+    }
+
 }
