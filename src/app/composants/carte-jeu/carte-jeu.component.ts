eb6dcbd0eca1fac67c8b9fe63c2a5f28b3dc65ae
Theo Journee <theo_journee@ens.univ-artois.fr>
2023-04-12 17:11:08 +0200

    #11 ajout des cartes

--- src/app/composants/carte-jeu/carte-jeu.component.ts
+++ src/app/composants/carte-jeu/carte-jeu.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Jeu} from '../../../models/jeu';
+import {JeuxService} from '../../services/jeux.service';
+import {ActivatedRoute} from "@angular/router";
+import {DataSourceAsynchro} from "../tableau-jeu/tableau-jeu.component";
 
 @Component({
   selector: 'app-carte-jeu',
-  templateUrl: './carte-jeu.component.html',
-  styleUrls: ['./carte-jeu.component.css']
+  template: `
+    <div class="card" *ngFor="let jeu of jeux$ | async">
+      <mat-card class="card">
+        <mat-card-header>
+          <div mat-card-avatar class="header-image"></div>
+          <mat-card-title>{{ jeu.nom }}</mat-card-title>
+          <mat-card-subtitle>Langue: {{jeu.langue | slice:0:2 }}</mat-card-subtitle>
+        </mat-card-header>
+        <img mat-card-lg-image [src]="jeu.url_media" [alt]="jeu.nom">
+        <mat-card-content>
+          <br>
+          <span class="categorie"> Catégorie: {{jeu.categorie_id}}</span>
+        </mat-card-content>
+        <span class="theme"> Thème: {{jeu.theme_id}}</span>
+      </mat-card>
+
+    </div>
+  `,
+  styles: [
+    `.card {
+      display: inline-block;
+      vertical-align: top;
+      margin-left: 45px;
+      margin-top: 20px;
+    }
+    `,
+  ],
 })
 export class CarteJeuComponent implements OnInit {
+  jeux$: Observable<Jeu[]> | undefined;
+  le_jeu: Observable<number> | undefined;
+  id: number = +(this.route.snapshot.paramMap.get('id') || 0);
 
-  constructor() { }
+  dataSource: DataSourceAsynchro = new DataSourceAsynchro(this.jeuxService)
+
+
+  constructor(private jeuxService: JeuxService,private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.jeux$= this.jeuxService.getJeux('asc', 2);
+    this.le_jeu = this.jeuxService.noteJeu(this.id);
+    this.dataSource = new DataSourceAsynchro(this.jeuxService)
+    this.dataSource.setCommentaire();
   }
 
 }
